36b7cdcb998684c77f3988ee47b221e512058966
Matteo Romanello <matteo.romanello@gmail.com>
2010-07-16 14:01:40 +0100

    replaced some hardlinked path with variables

--- crex/crex.py
+++ crex/crex.py
@@ -5,8 +5,8 @@ from utils import *
 from crfpp_wrap import CRF_classifier
 from crfpp_wrap import *
 
-log_file="/56k/phd/code/python/crfx.log"
-logging.basicConfig(level=logging.DEBUG,format='%(asctime)s - %(name)s - [%(levelname)s] %(message)s',filename=log_file,datefmt='%a, %d %b %Y %H:%M:%S',filemode='w')
+LOG_FILE="/56k/phd/code/python/crfx.log"
+logging.basicConfig(level=logging.DEBUG,format='%(asctime)s - %(name)s - [%(levelname)s] %(message)s',filename=LOG_FILE,datefmt='%a, %d %b %Y %H:%M:%S',filemode='w')
 logger = logging.getLogger('CRFX')
 
 pp = pprint.PrettyPrinter(indent=5)
